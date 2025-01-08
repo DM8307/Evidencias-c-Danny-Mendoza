0a43e93a0be0abc2aa9ddfd529853f9616c097d8
Danny Mendoza <dannyemendoza@hotmail.com>
2025-01-08 12:25:33 -0400

    completa

--- Evidencias c#/Actividad 1 tipos de datos/Program.cs
+++ Evidencias c#/Actividad 1 tipos de datos/Program.cs
@@ -1,2 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("Cual es tu primer nombre? ");
+            Console.Write("Escribe tu primer Nombre ");
+            string primernombre;
+            primernombre = Console.ReadLine();
+
+            /*
+            Console.Write("Cual es tu apellido? ");
+            Console.Write("Escribe tu apellido: ");
+            string Apellido;
+            Apellido = Console.ReadLine();
+            */
+
+            Console.Write("Escribe tu apellido: ");
+            string Apellido = Console.ReadLine();
+
+            Console.WriteLine("Hola, " + primernombre + " " + Apellido);
+            Console.ReadLine();
+
+        }
+    }
+}

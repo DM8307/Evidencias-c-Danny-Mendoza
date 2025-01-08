8ffd16533f15dad72ed981f9135053518328cb45
Danny Mendoza <dannyemendoza@hotmail.com>
2025-01-08 14:07:02 -0400

    nuevas actividades realizadas

---
+++ Evidencias c#/Actividad 11 Entendiendo el alcance/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_entendiendo_el_alcance
+{
+    class Program
+    {
+        private static string k = "";
+
+        static void Main(string[] args)
+        {
+            string j = "";
+
+            for (int i = 0; i < 10; i++)
+            {
+                j = i.ToString();
+                Console.WriteLine(i);
+
+                if (i == 9)
+                {
+                    string l = i.ToString();
+                }
+                //Console.WriteLine(l);     //out of scope
+            }
+
+            //Console.WriteLine(i);     //out of scope
+            Console.WriteLine("Afuera de los parametros para: " + j);
+            Console.WriteLine("Fuera del for de for: " + k);
+
+            HelperMethod();
+
+            Car myCar = new Car();
+            myCar.DoSomething();
+
+            Console.ReadLine();
+        }
+
+        static void HelperMethod()
+        {
+            Console.WriteLine("Valor de K de HelperMethod(): " + k);
+
+        }
+
+
+        class Car
+        {
+            public void DoSomething()
+            {
+                Console.WriteLine(helperMethod());
+            }
+
+            private string helperMethod()
+            {
+                return "Hello world!";
+            }
+        }
+    }
+}
---
+++ Evidencias c#/Actividad 13 Referencias/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_NamespacesAndReferencesAssemblies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.IO.StreamReader myStreamReader = new System.IO.StreamReader();
+            StreamReader mystreamReader = new StreamReader();
+        }
+    }
+}
